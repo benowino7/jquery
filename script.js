c8bb1245c4e6fd8610b482133575ff4f4c5f510d
benson7-coder <bensonowino7@gmail.com>
2019-12-05 14:45:35 +0300

    added img files,script file and style sheet

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("button#green").click(function () {
+        $("body").removeClass();
+        $("body").addClass("green-background");
+    });
+    $("button#yellow").click(function () {
+        $("body").removeClass();
+        $("body").addClass("yellow-background");
+    });
+    $("button#red").click(function () {
+        $("body").removeClass();
+        $("body").addClass("red-background");
+    });
+});
+
+
+
+
+
+
+//function name(b){
+            
+  //  alert ("Hi "+b);
+//}
+//b=prompt("May I have the pleasure of knowing your name");
+//name(b);
